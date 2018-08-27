223db183c6db5186e5fc16a9e2276ac12cee1386
rudwnok123 <rudwnok123@naver.com>
2018-08-28 03:18:07 +0900

    js를 이곳에서 하겠다고 선언

--- test.js
+++ test.js
@@ -1,17 +1,34 @@
-
-function a(){
-  global.luckyuNumber = 23
-}
-
-function b(){
-  global.luckyuNumber = 42
-}
-
-a();
-b();
+//앞으로의 js테스트는 이 폴더에서 한다
 
 
-// let b = a > 3 ? 'aa' : 'bb';
-// console.log(b);
+// function a() {
+//   global.luckyuNumber = 23
+// }
+
+// function b() {
+//   global.luckyuNumber = 42
+// }
+
+// a();
+// b();
+
+
+
+// ==========
+// 3항 연산자
+// ==========
+let number1 = 4;
+let returnN = number1 > 3 ? 'true' : 'false';
+console.log(returnN);
+// 리턴값을 반환
+
+
+// ==========
+// arrow function
+// ==========
+var name = 2;
 let sayHello = (name) => console.log('Hello', name);
-let func = () => {console.log('aa')};
+sayHello();
+
+let func = () => { console.log('aa') };
+func();
