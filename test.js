1af1b74094a29d4338ee8fa0c7a8c758693f44de
rudwnok123 <rudwnok123@naver.com>
2019-01-28 22:15:02 +0900

    selector test2

--- test.js
+++ test.js
@@ -1,11 +1,31 @@
 // jqeury처럼 $ 쓰기
-function a (dom) {
-  let dom = document.querySelector(`${dom}`);
-  return console.log(`${dom}`);
-}
-
-// a('#e_name');
+// function a (dom) {
+//   let dom = document.querySelector(`${dom}`);
+//   return console.log(`${dom}`);
+// }
 
 // a('#e_name').addEventListener('click',function(){
 //   alert('성공');
 // })
+
+function $(selector){
+	if ( ! (this instanceof $) ){
+		return new $(selector);
+	}
+	this.selector = document.querySelector(selector);
+	return this;
+}
+$.fn = $.prototype;
+$.fn.css = function(type, val) {
+    const isStyle = this.selector.getAttribute('style');
+    if (isStyle) {
+        this.selector.setAttribute('style', [isStyle, ';',type, ':', val].join(''))
+    } else {
+        this.selector.setAttribute('style', [type, ':', val].join(''));
+    }
+    return this;
+}
+let el = $('.test');
+console.log(el);
+
+$('.test').css('display', 'block').css('color', 'red').css('width', '200px');
