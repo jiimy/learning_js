761c71cafeea9c3484422dc1be6e0a19b107088a
rudwnok123 <rudwnok123@naver.com>
2019-03-19 03:32:43 +0900

    한개일때의 dom 선택은 되나 여러개일때는 어떻게..?

--- test.js
+++ test.js
@@ -1,31 +1,59 @@
-// jqeury처럼 $ 쓰기
-// function a (dom) {
-//   let dom = document.querySelector(`${dom}`);
-//   return console.log(`${dom}`);
-// }
+//*-------  jqeury처럼 $ 쓰기
+// dom 선택
+function $(...dom) {
+  // 여러개의 dom 받을 경우
+  var split = dom[0].split(',');
+  console.log('분할'+split[0], split[1]);
+  // console.log(split.length);
+  if(split.length > 1){
+    for ( i in split){
+      console.log(split[i]);
+      var domSelect = document.querySelector(split[i]);
+    }
+  }
+  var domSelect = document.querySelector(dom);
+  return domSelect;
+}
 
-// a('#e_name').addEventListener('click',function(){
-//   alert('성공');
+// css변경
+// addClass, removeClass, toggleClass
+$('.test, .test2').addEventListener('click', function(){
+  console.log('click');
+});
+// $('.test2').addEventListener('click',function(){
+//   console.log('click2');
 // })
 
-function $(selector){
-	if ( ! (this instanceof $) ){
-		return new $(selector);
-	}
-	this.selector = document.querySelector(selector);
-	return this;
-}
-$.fn = $.prototype;
-$.fn.css = function(type, val) {
-    const isStyle = this.selector.getAttribute('style');
-    if (isStyle) {
-        this.selector.setAttribute('style', [isStyle, ';',type, ':', val].join(''))
-    } else {
-        this.selector.setAttribute('style', [type, ':', val].join(''));
-    }
-    return this;
-}
-let el = $('.test');
-console.log(el);
+// $('.test, .test2').click(function(){
+//   console.log('click');
+// })
 
-$('.test').css('display', 'block').css('color', 'red').css('width', '200px');
+
+// $셀렉터로 css운용하기
+// function $(selector) {
+//   if (!(this instanceof $)) {
+//     return new $(selector);
+//   }
+//   this.selector = document.querySelector(selector);
+//   return this;
+// }
+// $.fn = $.prototype;
+// $.fn.css = function (type, val) {
+//   const isStyle = this.selector.getAttribute("style");
+//   if (isStyle) {
+//     this.selector.setAttribute(
+//       "style",
+//       [isStyle, ";", type, ":", val].join("")
+//     );
+//   } else {
+//     this.selector.setAttribute("style", [type, ":", val].join(""));
+//   }
+//   return this;
+// };
+// let el = $(".test");
+// console.log(el);
+
+// $(".test")
+//   .css("display", "block")
+//   .css("color", "red")
+//   .css("width", "200px");
