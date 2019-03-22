5eff9f0bedaec2d79f999cec10bdddcc96b5814c
rudwnok123 <rudwnok123@naver.com>
2019-03-22 19:59:54 +0900

    testcode init

---
+++ test_code.js
@@ -0,0 +1,26 @@
+// test code sample
+function test(title, testCode) {
+  try {
+    testCode();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function expect(result) {
+  return {
+    toBe: function(expected) {
+      if (result !== expected) {
+      	throw new Error(result + ' is not equal to ' + expected);
+      }
+    }
+  }
+}
+
+test('sum(1, 2) is not equal 5', function() {
+  expect(sum(1, 2)).toBe(3);
+});
+
+function sum (a,b){
+  return a+b;
+}
